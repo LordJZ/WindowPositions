07074fb5a7d4a3c70df3655b9b4d14af471a19d7
LordJZ <LordJZ@outlook.com>
2014-10-13 20:53:13 +0400

    Unwrap WPF class name pattern

--- WindowPositions/WindowDTO.cs
+++ WindowPositions/WindowDTO.cs
@@ -1,15 +1,28 @@
-﻿using LordJZ.WinAPI;
+﻿using System.Text.RegularExpressions;
+using LordJZ.WinAPI;
 
 namespace WindowPositions
 {
     public class WindowDTO
     {
+        //HwndWrapper[WindowPositions.exe;;837cb3a7-028d-43a7-bfee-33e6db4b1105]|MainWindow
+        static readonly Regex s_hwndWrapperRegex = new Regex(@"HwndWrapper\[(.+);[a-z0-9\-]{36}\]");
+
         public WindowDTO(NativeWindow window)
         {
             NativeWindow = window;
 
-            this.ClassName = window.ClassName;
+            this.ClassName = UnwrapClassName(window.ClassName);
             this.Title = window.Text;
+        }
+
+        static string UnwrapClassName(string className)
+        {
+            Match match = s_hwndWrapperRegex.Match(className);
+            if (match.Success)
+                return match.Groups[1].Value;
+
+            return className;
         }
 
         internal readonly NativeWindow NativeWindow;
